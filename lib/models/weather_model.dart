112da818c355be787194ef3f93d6dc4df838b812
Rami Leghrib <95674555+RamiLGH8@users.noreply.github.com>
2023-11-09 21:53:53 +0100

    new commit!

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -2,11 +2,28 @@ class WeatherModel {
   final double temp;
   final String city;
   final String desc;
+  final DateTime time;
+  final DateTime sunRise; 
+  final DateTime sunSet; 
 
-  WeatherModel({required this.temp, required this.city, required this.desc});
+  WeatherModel(
+      {required this.temp,
+      required this.city,
+      required this.desc,
+      required this.time,
+      required this.sunRise,
+      required this.sunSet
+      });
 
   WeatherModel.fromMap(Map<String, dynamic> json)
       : city = json['name'],
         temp = json['main']['temp'],
-        desc = json['weather'][0]['description'];
+        desc = json['weather'][0]['description'],
+        time = DateTime.fromMillisecondsSinceEpoch(json['dt'] * 1000),
+        sunRise = DateTime.fromMillisecondsSinceEpoch(json['sys']['sunrise'] * 1000),
+        sunSet = DateTime.fromMillisecondsSinceEpoch(json['sys']['sunset'] * 1000);
+
+  String formattedTime(DateTime time) {
+    return '${time.hour}:${time.minute}';
+  }
 }
--- lib/screen/weather_page.dart
+++ lib/screen/weather_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../models/weather_model.dart';
 import '../services/weather_service.dart';
+import 'package:lottie/lottie.dart';
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({Key? key});
@@ -14,18 +15,60 @@ class _WeatherPageState extends State<WeatherPage> {
       WeatherService(apiKey: '6708f4e44d161b6f51e1866d588a406c');
   WeatherModel? _weather;
 
-
   fetchWeather() async {
-   
-      String city = await _weatherService.getCurrentLocation();
-      print(city);
-      final weather = await _weatherService.getWeather(city);
-      print(weather);
-      setState(() {
-        _weather = weather;
-      });
-      print(_weather?.city);
-   
+    String city = await _weatherService.getCurrentLocation();
+    print(city);
+    final weather = await _weatherService.getWeather(city);
+
+    setState(() {
+      _weather = weather;
+    });
+    print(weather.formattedTime(_weather!.time));
+    print(_weather?.city);
+  }
+
+  String getLottieAsset(
+      String desc, String time, String sunRise, String sunSet) {
+    print(desc);
+    print(time);
+    print(sunRise);
+    print(sunSet);
+    if (desc.toLowerCase().contains('clouds')) {
+      return 'assets/cloudy.json';
+    }
+    if (desc.toLowerCase().contains('rain')) {
+      return 'assets/rainy.json';
+    }
+    if (desc.toLowerCase().contains('thunder')) {
+      return 'assets/thunder.json';
+    }
+    if (desc.toLowerCase().contains('snow')) {
+      return 'assets/snow.json';
+    }
+    // if (desc.toLowerCase().contains('clear') &&
+    //     time.compareTo(sunRise) == 1 &&
+    //     time.compareTo(sunSet) == -1) {
+    //   return 'assets/sunny.json';
+    // }
+    if (desc.toLowerCase().contains('clear') && time.compareTo(sunRise) == -1) {
+      return 'assets/night_clear.json';
+    }
+
+    switch (desc.toLowerCase()) {
+      case 'mist':
+      case 'smoke':
+      case 'haze':
+      case 'dust':
+      case 'fog':
+        return 'assets/cloudy.json';
+      case 'drizzle':
+        return 'assets/rainy.json';
+      default:
+        if (time.compareTo(sunRise) == 1 && time.compareTo(sunSet) == -1)
+          return 'assets/sunny.json';
+        else
+          return 'assets/night_clear.json';
+    }
   }
 
   @override
@@ -36,21 +79,31 @@ class _WeatherPageState extends State<WeatherPage> {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: SafeArea(
         child: Scaffold(
           body: _weather == null
-                  ? Center(child: CircularProgressIndicator())
-                  : Center(
-                      child: Column(
-                        children: [
-                          Text(_weather?.city ?? 'Loading...'),
-                          Text(_weather?.desc ?? 'Loading...'),
-                          Text(_weather?.temp.toString() ?? 'Loading...'),
-                        ],
-                      ),
+              ? Center(child: CircularProgressIndicator())
+              : Container(
+                  width: width,
+                  color: Colors.blue,
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(_weather!.city),
+                        Text(_weather!.temp.toInt().toString() + '°C'),
+                        Lottie.asset(getLottieAsset(
+                            _weather!.desc,
+                            _weather!.formattedTime(_weather!.time),
+                            _weather!.formattedTime(_weather!.sunRise),
+                            _weather!.formattedTime(_weather!.sunSet))),
+                      ],
                     ),
+                  ),
+                ),
         ),
       ),
     );
